daa4ce144c1cab6686c6ddcd99e416cf87ad2ab3
Steve Petusky <steve.petusky@gmail.com>
2026-05-26 09:48:35 -0400

    chore: add Firebase config, Firestore rules, Cloud Functions scaffold

---
+++ functions/lib/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = require("firebase-admin/app");
+(0, app_1.initializeApp)();
+// Collectors will be exported here as they are built
+// export { semanticScholarCollector } from './collectors/semantic-scholar';
+// export { githubBiosCollector } from './collectors/github-bios';
+// etc.
+// AI Brain
+// export { aiBrain } from './ai/brain';
+//# sourceMappingURL=index.js.map
